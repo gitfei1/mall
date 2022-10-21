beb71773a1966b9e3c4976c7961b67eaa011492a
gitfei1 <2632608493@qq.com>
2022-10-21 18:54:17 +0800

    feat:user&product

---
+++ service/user/rpc/internal/logic/registerlogic.go
@@ -0,0 +1,63 @@
+package logic
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc/status"
+	"mall/common/cryptx"
+	"mall/service/user/model"
+	"mall/service/user/rpc/internal/svc"
+	"mall/service/user/rpc/types/user"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type RegisterLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *RegisterLogic {
+	return &RegisterLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *RegisterLogic) Register(in *user.RegisterRequest) (*user.RegisterResponse, error) {
+	// todo: add your logic here and delete this line
+	fmt.Println("Register in ")
+	_, err := l.svcCtx.UserModel.FindOneByMobile(l.ctx, in.Mobile)
+	if err == nil {
+		return nil, status.Error(100, "该用户已存在")
+	}
+	if err == model.ErrNotFound {
+		newUser := model.User{
+			Name:     in.Name,
+			Gender:   in.Gender,
+			Mobile:   in.Mobile,
+			Password: cryptx.PasswordEncrypt(l.svcCtx.Config.Salt, in.Password),
+		}
+		res, err := l.svcCtx.UserModel.Insert(l.ctx, &newUser)
+		if err != nil {
+			fmt.Println("Register err ", err)
+			return nil, status.Error(500, err.Error())
+		}
+		newUser.Id, err = res.LastInsertId()
+		if err != nil {
+			fmt.Println("Register Id err ", err)
+			return nil, status.Error(500, err.Error())
+		}
+		fmt.Println("Register ss ")
+		return &user.RegisterResponse{
+			Id:     newUser.Id,
+			Name:   newUser.Name,
+			Gender: newUser.Gender,
+			Mobile: newUser.Mobile,
+		}, nil
+	}
+	fmt.Println("Register end err ", err)
+	return nil, status.Error(500, err.Error())
+}
